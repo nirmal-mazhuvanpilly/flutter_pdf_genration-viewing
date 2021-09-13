8a27e0e19de63c247cfb67b794821ac2d915923c
nirmal-mazhuvanpilly <nirmalmazhuvanpilly@gmail.com>
2021-09-13 17:07:58 +0530

    Total Page Number,Current Page Number,Jump to Page - Added

--- lib/view/native_pdf_preview.dart
+++ lib/view/native_pdf_preview.dart
@@ -12,13 +12,15 @@ class NativePdfPreviewScreen extends StatefulWidget {
 class _NativePdfPreviewScreenState extends State<NativePdfPreviewScreen> {
   PdfController pdfController;
 
+  int actualPageNumber = 1, allPagesCount = 0;
+
   @override
   void initState() {
     super.initState();
     pdfController = PdfController(
       document: PdfDocument.openFile(widget.path),
       initialPage: 1,
-      viewportFraction: 1,
+      viewportFraction: 0.80,
     );
   }
 
@@ -28,9 +30,55 @@ class _NativePdfPreviewScreenState extends State<NativePdfPreviewScreen> {
       appBar: AppBar(
         title: Text("Native Preview"),
       ),
-      body: PdfView(
-        controller: pdfController,
-        scrollDirection: Axis.vertical,
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                IconButton(
+                    icon: Icon(Icons.arrow_circle_down),
+                    onPressed: () {
+                      if (actualPageNumber < allPagesCount)
+                        pdfController.jumpToPage(actualPageNumber + 1);
+                    }),
+                IconButton(
+                    icon: Icon(Icons.arrow_circle_up_outlined),
+                    onPressed: () {
+                      if (actualPageNumber > 1)
+                        pdfController.jumpToPage(actualPageNumber - 1);
+                    }),
+                Text(actualPageNumber.toString()),
+                Text("/"),
+                Text(allPagesCount.toString()),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.black87,
+              child: PdfView(
+                controller: pdfController,
+                scrollDirection: Axis.vertical,
+                physics: BouncingScrollPhysics(),
+                onDocumentLoaded: (document) {
+                  setState(() {
+                    allPagesCount = document.pagesCount;
+                    print(allPagesCount);
+                  });
+                },
+                onPageChanged: (page) {
+                  setState(() {
+                    actualPageNumber = page;
+                    print(actualPageNumber);
+                  });
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
